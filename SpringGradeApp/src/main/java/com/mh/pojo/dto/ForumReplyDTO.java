a951b9eb0330c76d21262d492e515756828e311a
Yuh <huypham3188@gmail.com>
2025-04-20 16:57:58 +0700

    forum reply admin (not done)

---
+++ SpringGradeApp/src/main/java/com/mh/pojo/dto/ForumReplyDTO.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mh.pojo.dto;
+
+import com.mh.pojo.ForumReply;
+import java.util.Date;
+
+/**
+ *
+ * @author leoma
+ */
+public class ForumReplyDTO {
+
+    private int id;
+    private String content;
+    private String image;
+    private Date createdDate;
+    private Date updatedDate;
+    private String user;
+    private int parentId;
+
+    public ForumReplyDTO(ForumReply reply) {
+        this.id = reply.getId();
+        this.content = reply.getContent();
+        this.image = reply.getImage();
+        this.createdDate = reply.getCreatedDate();
+        this.updatedDate = reply.getUpdatedDate();
+        this.user = String.format("%d - %s", reply.getUser().getId(), reply.getUser().getFirstName());
+        this.parentId = reply.getParent() != null ? reply.getParent().getId() : null;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /**
+     * @return the createdDate
+     */
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    /**
+     * @param createdDate the createdDate to set
+     */
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    /**
+     * @return the updatedDate
+     */
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    /**
+     * @param updatedDate the updatedDate to set
+     */
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    /**
+     * @return the parentId
+     */
+    public int getParentId() {
+        return parentId;
+    }
+
+    /**
+     * @param parentId the parentId to set
+     */
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+}
